5285ead7c89684820985bf24d6b1cd8af28efdb9
ritamargo25 <daisypearl@mail.ru>
2022-11-29 00:31:02 +0300

    innit commit

---
+++ array.js
@@ -0,0 +1,27 @@
+/*1. Напишите функцию `sumInput()`, которая:
+    - Просит пользователя ввести значения, используя текстовое поле `input` и сохраняет их в массив
+    - Когда пользователь нажмёт кнопку «Готово»
+        - Выводит получившийся массив в отсортированном по возрастанию виде
+        - Подсчитывает и возвращает сумму элементов массива*/
+
+
+
+function sumInput() {
+    let numbers = [];
+    let a = document.querySelector('.num1');
+    let b = document.querySelector('.num2');
+    let c = document.querySelector('.num3');
+    let d = document.querySelector('.num4');
+    numbers.push(a, b, c, d);
+    numbers.sort()
+    let sum = 0;
+    sum = +number;
+    return sum;
+}
+alert(sumInput());
+
+
+
+
+
+
---
+++ tests.js
@@ -0,0 +1,29 @@
+//- 1. Дан массив`['js', 'css', 'html']`.Выведите на экран первый элемент.
+
+let frontEnd = ['js', 'css', 'html'];
+alert(frontEnd[0]);
+
+//- 2. Отфильтруйте массив `[0, 1, false, 2, undefined, '', 3, null]` 
+//от нежелательных значений, таких как`false`, `undefined`, пустые строки, `0`, `null` с помощью метода`filter`.
+//Ожидаемый результат: `[1, 2, 3]`.
+
+let items = [0, 1, false, 2, undefined, '', 3, null];
+let result = items.filter(function (elem) {
+    if (elem > 0) {
+        return true;
+    }
+});
+console.log(result);
+
+
+//- 3. Дан массив`[[1,2], [1,2,3], [1,2,3,4]]`.Найдите индекс массива, длина которого`> 3`.
+//Ожидаемый результат: `2`
+
+let array = [
+    [1, 2],
+    [1, 2, 3],
+    [1, 2, 3, 4]
+];
+console.log(array.indexOf(array.lenght > 3));
+
+
